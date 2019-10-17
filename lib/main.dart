81a7ecc8fc2e2cbf24369f2c28a906ebf27f640b
makiintoch <makiintoch@gmail.com>
2019-10-17 16:57:09 +0300

    add transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -16,6 +18,20 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -23,13 +39,21 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Personal expenses'),
       ),
-      body: Column(children: <Widget>[
-        Card(
-          child: Text('CHART!'),
-          elevation: 5,
-        ),
-        Card(child: Text('LIST OF TX'),)
-      ],),
+      body: Column(
+        children: <Widget>[
+          Card(
+            child: Text('CHART!'),
+            elevation: 5,
+          ),
+          Column(
+            children: transactions.map((transaction) {
+              return Card(
+                child: Text(transaction.title),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
     );
   }
 }
